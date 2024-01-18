2758c6cb22dbd964fdacb19dc683f03cf1eb7e7a
Davide Baldi <d.baldi88@gmail.com>
2024-01-18 18:13:18 +0100

    break

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 $(()=>{
+//Hamburger menu 1st level
+$("nav .hamburger-menu").on("click", event => $(event.target).next("ul").toggle());
+$("nav .hamburger-menu ul li").on({
+    "mouseenter": event => $(event.target).toggleClass("active"),
+    "mouseleave": event => $(event.target).toggleClass("active"),
+    "click": event=> {
+        $(event.target).find(".submenu").toggle("active");
+        $(event.target).siblings().find(".submenu").hide("active");
 
-$(".hamburger-menu").on("click", event => $(event.target).next("ul").toggle())
+}
+})
+
 
 
 
